76cc8fe4899ee03ca908ffdf3a057b73d86299d1
Muhammad Hamza <muhammadhamza14210@gmail.com>
2024-01-30 20:07:40 -0700

    Adding blue color to shape class

--- app/src/main/java/com/example/cmput301_w24_t54/Shape.java
+++ app/src/main/java/com/example/cmput301_w24_t54/Shape.java
@@ -3,11 +3,6 @@ package com.example.cmput301_w24_t54;
 public class Shape {
     private int x;
     private int y;
-    private String color;
-
-    // Constructor to initialize the color to "Blue"
-    public Shape() {
-        color = "Blue";
-    }
+    private String color = "Blue";
 }
 
