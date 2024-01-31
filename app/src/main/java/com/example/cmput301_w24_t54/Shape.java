aaa8df5ee2fd14fe49b5db49174bb83e64ce0e50
Muhammad Hamza <muhammadhamza14210@gmail.com>
2024-01-30 18:12:20 -0700

    first commit

---
+++ app/src/main/java/com/example/cmput301_w24_t54/Shape.java
@@ -0,0 +1,7 @@
+package com.example.cmput301_w24_t54;
+
+public class Shape {
+    private int x;
+    private int y;
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CMPUT301-W24-T54"
+include(":app")
+ 
